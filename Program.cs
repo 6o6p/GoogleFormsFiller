6f2af979ce48b209cc888264a0383e6ccd064dfa
Фадихин Дмитрий <fadixin@gmail.com>
2022-03-29 22:54:50 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GoogleFormsFiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var responseBodyAsText = Run(new HttpClient()).Result;
+            responseBodyAsText = responseBodyAsText.Replace(">", ">\n");
+
+            Console.WriteLine(responseBodyAsText);
+        }
+
+        public static async Task<string> Run(HttpClient httpClient)
+        {
+            HttpResponseMessage response = await httpClient.GetAsync("https://docs.google.com/forms/d/e/1FAIpQLScn6pTePXTW0LodjrNYNXZT3jBZDUeedJh2WE7m2d1IJ5Ttww/viewform");
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}

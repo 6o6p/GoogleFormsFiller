245e692e282c9714780574462eae2a78c26effcd
Фадихин Дмитрий <fadixin@gmail.com>
2022-04-04 22:15:45 +0400

    Роботат
    данные в форму отправляет. Делай всё остальное

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Security.Authentication.ExtendedProtection;
 using System.Threading.Tasks;
 
 namespace GoogleFormsFiller
@@ -20,11 +23,13 @@ namespace GoogleFormsFiller
             var uri = new Uri("https://docs.google.com/forms/d/e/1FAIpQLScn6pTePXTW0LodjrNYNXZT3jBZDUeedJh2WE7m2d1IJ5Ttww/viewform");
 
 
-
-
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("entry.1967548713", "shit"),
+            });
             HttpResponseMessage response = await httpClient.GetAsync(uri);
             response.EnsureSuccessStatusCode();
-
+            await httpClient.PostAsync(new Uri("https://docs.google.com/forms/d/e/1FAIpQLScn6pTePXTW0LodjrNYNXZT3jBZDUeedJh2WE7m2d1IJ5Ttww/formResponse"), content);
 
             return await response.Content.ReadAsStringAsync();
         }
